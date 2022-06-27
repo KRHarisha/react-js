55e2f0e5d4c81320a196e69c9d373339895b1c34
KRHarisha <105624826+KRHarisha@users.noreply.github.com>
2022-06-27 13:20:13 +0530

    Add files via upload

---
+++ react-meme-generator/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./style.css"
+import Header  from "./components/Header"
+import Meme from "./components/Meme"
+/* challenge: Build the Header component */
+/**
+ * Challenge: 
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ * - Don't worry about adding any functionality yet
+ */
+export default function App()
+{
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+    
+  )
+}
